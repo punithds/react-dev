7a511a4220b2a7d1c5066e9689b537c56eeac228
punithds <kapunithkumar@gmail.com>
2021-08-07 22:57:09 +0530

    updatad app with collection preview and master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
 import { Homepage } from './pages/homepage/homepage.component';
+import ShopPage from "./pages/shop/shop.components";
+
 
 function App() {
   return (
     <div className="App">
-     <Homepage/>
+    <Switch>
+    <Route exact path="/" component={Homepage}/>
+    <Route exact path="/shop" component={ShopPage}/>
+    </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import "./menu-item.styles.scss"
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import "./menu-item.styles.scss";
 
-export const MenuItem = ({key,imageUrl,title,size}) => {
+
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) => {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{
             backgroundImage:`url(${imageUrl})`
         }}>
@@ -17,3 +19,5 @@ export const MenuItem = ({key,imageUrl,title,size}) => {
 
         )
 }
+
+export default withRouter(MenuItem);
