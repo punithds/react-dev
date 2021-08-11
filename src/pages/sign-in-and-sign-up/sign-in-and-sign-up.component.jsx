35ae2ac09304a87dce6dc2ca84adde46102d14c3
punithds <kapunithkumar@gmail.com>
2021-08-11 08:51:19 +0530

    created signin and signup with email

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.components';
+import "./sign-in-sign-up.styles.scss"
 
 export const SignInAndSignUpPage = () => {
     return (
         <div className="sign-in-and-sign-up">
         <SignIn/>
+        <SignUp/>
         </div>
     )
 }
